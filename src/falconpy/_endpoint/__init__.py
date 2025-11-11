e1326f42ba4aa25c18a0f2c6ab5de0ff1cf3f72c
Alexander Moomaw <alhumaw47@gmail.com>
2025-11-11 13:03:39 -0800

    Adding deprecated CloudSecurity operations.

--- src/falconpy/_endpoint/__init__.py
+++ src/falconpy/_endpoint/__init__.py
@@ -58,6 +58,7 @@ from ._cloud_oci_registration import _cloud_oci_registration_endpoints
 from ._cloud_policies import _cloud_policies_endpoints
 from ._cloud_security import _cloud_security_endpoints
 from ._cloud_security_assets import _cloud_security_assets_endpoints
+from ._cloud_security_compliance import _cloud_security_compliance_endpoints
 from ._cloud_connect_aws import _cloud_connect_aws_endpoints
 from ._cloud_snapshots import _cloud_snapshots_endpoints
 from ._container_image_compliance import _container_image_compliance_endpoints
@@ -156,6 +157,7 @@ api_endpoints.extend(_cloud_oci_registration_endpoints)
 api_endpoints.extend(_cloud_policies_endpoints)
 api_endpoints.extend(_cloud_security_endpoints)
 api_endpoints.extend(_cloud_security_assets_endpoints)
+api_endpoints.extend(_cloud_security_compliance_endpoints)
 api_endpoints.extend(_cloud_snapshots_endpoints)
 api_endpoints.extend(_container_image_compliance_endpoints)
 api_endpoints.extend(_configuration_assessment_evaluation_logic_endpoints)
@@ -246,6 +248,7 @@ deprecated_endpoints.extend(_cloud_aws_registration_deprecated)
 deprecated_endpoints.extend(_cloud_azure_registration_deprecated)
 deprecated_endpoints.extend(_cloud_oci_registration_deprecated)
 deprecated_endpoints.extend(_cloud_security_assets_deprecated)
+deprecated_endpoints.extend(_cloud_security_endpoints)
 deprecated_endpoints.extend(_correlation_rules_deprecated)
 deprecated_endpoints.extend(_certificate_based_exclusions_deprecated)
 deprecated_endpoints.extend(_custom_ioa_deprecated)
